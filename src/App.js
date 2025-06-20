78fa76e9fd4b90a80a2ab4a0d4ed1640f7a19b87
Chris85234 <christianalfaro530@gmail.com>
2025-06-19 18:47:32 -0600

    segundo inteno

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import { Routes, Route } from "react-router-dom";
-import Navbar from "../src/LandingPage/nabvar.js";
-import Home from "./Pages/home.js";
-import "../src/Styles/Landipage/navbar.css";
-import "../src/Styles/Landipage/energyCarrousel.css";
-import "../src/Styles/Landipage/energyServices.css";
-import "../src/Styles/Landipage/Hero.css";
-import "../src/Styles/Landipage/ourSolutions.css";
-import "../src/Styles/Landipage/propellerSection.css";
-import "../src/Styles/styles.css";
-
-
-  
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} /> 
-            </Routes>
-        </>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
