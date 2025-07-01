ef3f3967f57e18ecd561dbee50239ed9c41fb466
Jenniffer <jenniffermartinezcastro04@gmail.com>
2025-06-30 19:03:16 -0600

    Se agregó el Dashboard para los administradores

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Historial from "./Historial/Historial.jsx"
 import LandingPage from './LandingPage/Page.js'
+import AdminDash from './AdminDash/AdminDash.jsx'
 
 function App() {
   return (
     <div className="App">
       <main>
-        <LandingPage/>
+        <AdminDash/>
       </main>
     </div>
   );
